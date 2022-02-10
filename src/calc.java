f48ac27261643dc09b0c2ca49a679dfc5473c121
Dhruvi Patel <DhruviPatel@dhruvis-mbp.quinnipiac.edu>
2022-02-10 11:58:03 -0500

    Gave user option to multiply and divide

--- src/calc.java
+++ src/calc.java
@@ -10,17 +10,31 @@ public class calc {
 		int num2 = scanner.nextInt();
 		
 		Scanner question1 = new Scanner(System.in);
-		System.out.println("Pick pick from the options: \n Add \n Subtract");
+		System.out.println("Pick pick from the options: \n Add \n Subtract \n Multiply \n Divide");
 		String answer1 = question1.nextLine();
 		
 		if (answer1.equalsIgnoreCase("Add")) {
 			System.out.println(num1 + num2);
 		} 
-		else 
+		else if  (answer1.equalsIgnoreCase("Subtract"))
 		{
 			System.out.println(num1 - num2);
 		}
+		else if (answer1.equalsIgnoreCase("Multiply")) 
+		{
+			System.out.println(num1 * num2);
+	
+		}else if (answer1.equalsIgnoreCase("Divide"))
+		{
+			
+			System.out.println(num1 / num2);
 		
+		} else {
+			
+			System.out.println("You did not enter one of the options");
+			System.exit(0);
+			
+		}
 		
 	}
 
