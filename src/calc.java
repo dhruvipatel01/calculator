5fda6e265b36f019bd39a5b0e8b40c3dc9bec65e
Dhruvi Patel <DhruviPatel@dhruvis-mbp.quinnipiac.edu>
2022-02-10 11:51:41 -0500

    Gave user option to add or subtract

--- src/calc.java
+++ src/calc.java
@@ -9,6 +9,18 @@ public class calc {
 		int num1 = scanner.nextInt();
 		int num2 = scanner.nextInt();
 		
+		Scanner question1 = new Scanner(System.in);
+		System.out.println("Pick pick from the options: \n Add \n Subtract");
+		String answer1 = question1.nextLine();
+		
+		if (answer1.equalsIgnoreCase("Add")) {
+			System.out.println(num1 + num2);
+		} 
+		else 
+		{
+			System.out.println(num1 - num2);
+		}
+		
 		
 	}
 
